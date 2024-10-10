0b8f19c0d208c4832e2fe8bf276ddb7ee507841f
Vladyslav Rehan <rehanvladyslav@gmail.com>
2024-10-11 02:01:04 +0300

    Fix system solving

--- lsys/src/thomas.cpp
+++ lsys/src/thomas.cpp
@@ -9,22 +9,22 @@ std::vector<double> Thomas::solve(
 	const std::vector<double>& c,
 	const std::vector<double>& d
 ) {
-	std::size_t N = d.size();
+	std::size_t N = b.size();
 
-	std::vector<double> c_star(N, 0.0);
-	std::vector<double> d_star(N, 0.0);
+	std::vector<double> c_star(N);
+	std::vector<double> d_star(N);
 
 	c_star[0] = c[0] / b[0];
 	d_star[0] = d[0] / b[0];
 
 	for (int i = 1; i < N; i++) {
-		double m = 1.0 / (b[i] - a[i]*c_star[i-1]);
-		c_star[i] = c[i]*m;
-		d_star[i] = (d[i] - a[i]*d_star[i-1])*m;
+		double m = (b[i] - a[i-1]*c_star[i-1]);
+		c_star[i] = c[i]/m;
+		d_star[i] = (d[i] - a[i-1]*d_star[i-1])/m;
 	}
 
 	std::vector<double> x(N);
-	x[N-1] = d_star[N-1];
+	x.back() = d_star.back();
 	for (int i = N-2; i >= 0; i--) {
 		x[i] = d_star[i] - c_star[i] * x[i+1];
 	}
@@ -32,3 +32,4 @@ std::vector<double> Thomas::solve(
 }
 
 }
+
--- lsys/tests/test1.cpp
+++ lsys/tests/test1.cpp
@@ -1,5 +1,20 @@
+#include <iomanip>
 #include <iostream>
 #include <thomas.hpp>
+
+bool comp(
+	const std::vector<double>& a, 
+	const std::vector<double>& b, 
+	double delta = 0.01) {
+	if (a.size() != b.size())
+		return false;
+
+	for (auto ait = a.begin(), bit = b.begin(); ait != a.end(); ait++, bit++)
+		if (std::abs(*ait - *bit) > delta)
+			return false;
+
+	return true;
+}
 
 int main() {
 
@@ -7,13 +22,20 @@ int main() {
 	a = {4, 5, 6, 9},
 	b = {6, 8, 5, 3, 11},
 	c = {9, 7, 3, 1},
-	l = {7, 3, 9, 4, 5};
+	d = {7, 3, 9, 4, 5};
 
-	std::vector<double> sol = nm::lsys::Thomas::solve(a, b, c, l);
+	std::vector<double> 
+	sol = nm::lsys::Thomas::solve(a, b, c, d),
+	expect = {635/1992.0, 563/996.0, -199/498.0, 2713/996.0, -589/332.0};
 
+	std::cout << "expected: ";
+	for (auto const& x : expect) {
+		std::cout << std::setw(6) << std::setprecision(3) << x << " ";
+	}
+	std::cout << "\nres:      ";
 	for (auto const& x : sol) {
-		std::cout << x << " ";
+		std::cout << std::setw(6) << std::setprecision(3) << x << " ";
 	}
 
-	return sol != std::vector<double>{635/1992.0, 563/996.0, -199/498.0, 2713/996.0, -589/332.0};
+	return !comp(sol, expect);
 }
