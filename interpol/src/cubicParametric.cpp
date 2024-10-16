5504ec92bcf84f9718639f28ae93306b50be5e70
Vladyslav Rehan <rehanvladyslav@gmail.com>
2024-10-16 22:28:53 +0300

    Fix parametric interpolation

--- interpol/src/cubicParametric.cpp
+++ interpol/src/cubicParametric.cpp
@@ -28,11 +28,11 @@ double CubicParametric::calculateS(double t, ListT& ms, ListT& il) {
 	std::size_t ti = static_cast<std::size_t>(t);
 	ti %= i_n;
 
-	if (ti == 0)
-		return ms[i_n]*std::pow(ti - t, 3)/6 +
-		ms[ti]*std::pow(t - (i_n), 3)/6 + 
-		(il[i_n-1] - ms[i_n-1]/6)*(ti - t) +
-		(il[ti] - ms[ti]/6)*(t - (i_n));
+	// if (ti == 0)
+	// 	return ms[i_n]*std::pow(ti - t, 3)/6 +
+	// 	ms[ti]*std::pow(t - (i_n), 3)/6 + 
+	// 	(il[i_n-1] - ms[i_n-1]/6)*(ti - t) +
+	// 	(il[ti] - ms[ti]/6)*(t - (i_n));
 
 
 	return ms[ti-1]*std::pow(ti - t, 3)/6 +
