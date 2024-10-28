da98deb51e207efea6d9447e9f0a76189562dde7
Vladyslav Rehan <rehanvladyslav@gmail.com>
2024-10-28 18:03:07 +0200

    Change Simpson formula

--- numeric/src/simpson.cpp
+++ numeric/src/simpson.cpp
@@ -3,26 +3,19 @@
 namespace ca::numeric {
 
 double Simpson::integrate() {
-	std::size_t N = s_x.size() - 1;
+	std::size_t n = s_x.size() - 1;
 
-	double result = 0.0;
-	for (std::size_t i = 1; i < N; i += 2) {
-		double h0 = h(i-1), h1 = h(i);
-		double hph = h1 + h0, hdh = h1/h0, hmh = h1*h0;
-		result += (hph/6)*((2 - hdh)*s_y[i-1] + 
-			(std::pow(hph, 2)/hmh)*s_y[i] + (2 - 1/hdh)*s_y[i+1]
-		);
+	double step = h(0);
+
+	double sum_odds = 0.0;
+	for (int i = 1; i < n; i += 2) {
+		sum_odds += s_y[i];
+	}
+	double sum_evens = 0.0;
+	for (int i = 2; i < n; i += 2) {
+		sum_evens += s_y[i];
 	}
 
-	if (N % 2 == 1) {
-		double h0 = h(N-2), h1 = h(N-1);
-		result += s_y[N]*(2*std::pow(h1, 2) + 3*h0*h1)/(6*(h0 + h1));
-		result += s_y[N-1]*(std::pow(h1, 2) + 3*h1*h0)/(6*h0);
-		result -= s_y[N-2]*std::pow(h1, 3)/(6*h0*(h0 + h1));
-	}
-
-	return result;
-}
-
+	return (s_y.front() + s_y.back() + 2 * sum_evens + 4 * sum_odds) * step / 3;}
 }
 
